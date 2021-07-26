78cb3ccbe5e8fde1da6e061cb7237cdf3177197b
Gabriele Santomaggio <g.santomaggio@gmail.com>
2021-07-26 17:42:59 +0200

    Add peer properties tests

--- protocol/src/commands/peer_properties.rs
+++ protocol/src/commands/peer_properties.rs
@@ -6,6 +6,7 @@ use crate::{
     error::{DecodeError, EncodeError},
     protocol::commands::COMMAND_PEER_PROPERTIES,
     types::CorrelationId,
+    ResponseCode,
 };
 
 use super::Command;
@@ -60,7 +61,9 @@ impl Command for PeerPropertiesCommand {
 
 #[derive(Debug, PartialEq)]
 pub struct PeerPropertiesResponse {
-    server_properties: HashMap<String, String>,
+    pub(crate) correlation_id: CorrelationId,
+    pub(crate) code: ResponseCode,
+    pub(crate) server_properties: HashMap<String, String>,
 }
 
 impl PeerPropertiesResponse {
@@ -71,18 +74,30 @@ impl PeerPropertiesResponse {
 
 impl Decoder for PeerPropertiesResponse {
     fn decode(input: &[u8]) -> Result<(&[u8], Self), DecodeError> {
+        let (input, correlation_id) = CorrelationId::decode(input)?;
+        let (input, code) = ResponseCode::decode(input)?;
         let (input, server_properties) = Self::decode_map(input)?;
 
-        Ok((input, PeerPropertiesResponse { server_properties }))
+        Ok((
+            input,
+            PeerPropertiesResponse {
+                correlation_id,
+                code,
+                server_properties,
+            },
+        ))
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::PeerPropertiesCommand;
     use std::collections::HashMap;
 
     use crate::codec::{Decoder, Encoder};
+    use crate::commands::peer_properties::PeerPropertiesResponse;
+
+    use super::PeerPropertiesCommand;
+    use crate::ResponseCode;
 
     #[test]
     fn peer_properties_request_test() {
@@ -105,6 +120,45 @@ mod tests {
         let (remaining, decoded) = PeerPropertiesCommand::decode(&buffer).unwrap();
 
         assert_eq!(peer_properties, decoded);
+
+        assert!(remaining.is_empty());
+    }
+
+    impl Encoder for PeerPropertiesResponse {
+        fn encoded_size(&self) -> u32 {
+            0
+        }
+
+        fn encode(
+            &self,
+            writer: &mut impl std::io::Write,
+        ) -> Result<(), crate::error::EncodeError> {
+            self.correlation_id.encode(writer)?;
+            self.code.encode(writer)?;
+            self.server_properties.encode(writer)?;
+            Ok(())
+        }
+    }
+
+    #[test]
+    fn open_response_test() {
+        let mut buffer = vec![];
+
+        let mut properties = HashMap::new();
+        properties.insert("server".to_owned(), "RabbitMQ server".to_owned());
+        properties.insert("Version ".to_owned(), "5.5.5.5".to_owned());
+
+        let peer_properties_response = PeerPropertiesResponse {
+            correlation_id: 1.into(),
+            code: ResponseCode::Ok,
+            server_properties: properties,
+        };
+
+        let _ = peer_properties_response.encode(&mut buffer);
+
+        let (remaining, decoded) = PeerPropertiesResponse::decode(&buffer).unwrap();
+
+        assert_eq!(peer_properties_response, decoded);
 
         assert!(remaining.is_empty());
     }
