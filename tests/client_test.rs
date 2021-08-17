155c3dbfb2c3125db88225bb60bee7ffaf162426
wolf4ood <enrico.risa@gmail.com>
2021-08-18 00:41:51 +0200

    Added test client for base client test with create and delete stream

--- tests/client_test.rs
+++ tests/client_test.rs
@@ -1,7 +1,9 @@
 use fake::{Fake, Faker};
 use rabbitmq_stream_client::{Client, ClientOptions};
 use rabbitmq_stream_protocol::ResponseCode;
+mod common;
 
+use common::TestClient;
 #[tokio::test]
 async fn client_connection_test() {
     let client = Client::connect(ClientOptions::default()).await.unwrap();
@@ -10,40 +12,24 @@ async fn client_connection_test() {
     assert_ne!(client.connection_properties().await.len(), 0);
 }
 
-#[tokio::test]
+#[tokio::test(flavor = "multi_thread")]
 async fn client_create_stream_test() {
-    let stream: String = Faker.fake();
-    let client = Client::connect(ClientOptions::default()).await.unwrap();
-
-    let response = client.create_stream(&stream).await.unwrap();
-
-    assert_eq!(&ResponseCode::Ok, response.code());
+    TestClient::create().await;
 }
-
-#[tokio::test]
+#[tokio::test(flavor = "multi_thread")]
 async fn client_create_stream_error_test() {
-    let stream: String = Faker.fake();
-    let client = Client::connect(ClientOptions::default()).await.unwrap();
-
-    // First ok
-    let response = client.create_stream(&stream).await.unwrap();
-    assert_eq!(&ResponseCode::Ok, response.code());
+    let test = TestClient::create().await;
 
     // Second ko
-    let response = client.create_stream(&stream).await.unwrap();
+    let response = test.client.create_stream(&test.stream).await.unwrap();
     assert_eq!(&ResponseCode::StreamAlreadyExists, response.code());
 }
 
-#[tokio::test]
+#[tokio::test(flavor = "multi_thread")]
 async fn client_delete_stream_test() {
-    let stream: String = Faker.fake();
-    let client = Client::connect(ClientOptions::default()).await.unwrap();
+    let test = TestClient::create().await;
 
-    let response = client.create_stream(&stream).await.unwrap();
-
-    assert_eq!(&ResponseCode::Ok, response.code());
-
-    let response = client.delete_stream(&stream).await.unwrap();
+    let response = test.client.delete_stream(&test.stream).await.unwrap();
 
     assert_eq!(&ResponseCode::Ok, response.code());
 }
---
+++ tests/common.rs
@@ -0,0 +1,29 @@
+use fake::{Fake, Faker};
+use rabbitmq_stream_client::{Client, ClientOptions};
+use rabbitmq_stream_protocol::ResponseCode;
+
+pub struct TestClient {
+    pub client: Client,
+    pub stream: String,
+}
+
+impl TestClient {
+    pub async fn create() -> TestClient {
+        let stream: String = Faker.fake();
+        let client = Client::connect(ClientOptions::default()).await.unwrap();
+
+        let response = client.create_stream(&stream).await.unwrap();
+
+        assert_eq!(&ResponseCode::Ok, response.code());
+        TestClient { client, stream }
+    }
+}
+
+impl Drop for TestClient {
+    fn drop(&mut self) {
+        tokio::task::block_in_place(|| {
+            tokio::runtime::Handle::current()
+                .block_on(async { self.client.delete_stream(&self.stream).await.unwrap() })
+        });
+    }
+}
