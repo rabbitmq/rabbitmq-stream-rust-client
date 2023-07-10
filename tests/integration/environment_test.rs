08373f3aca062919e4b40664fa8909634164fa19
Gabriele Santomaggio <G.santomaggio@gmail.com>
2023-07-10 19:55:29 +0200

    formatting
    Signed-off-by: Gabriele Santomaggio <G.santomaggio@gmail.com>

--- tests/integration/environment_test.rs
+++ tests/integration/environment_test.rs
@@ -1,6 +1,6 @@
 use fake::{Fake, Faker};
 
-use rabbitmq_stream_client::{Environment, error};
+use rabbitmq_stream_client::{error, Environment};
 use rabbitmq_stream_protocol::ResponseCode;
 
 use crate::common::TestEnvironment;
@@ -9,7 +9,6 @@ use crate::common::TestEnvironment;
 async fn environment_create_test() {
     let _ = TestEnvironment::create().await;
 }
-
 
 #[tokio::test(flavor = "multi_thread")]
 async fn environment_create_stream_twice() {
@@ -22,11 +21,13 @@ async fn environment_create_stream_twice() {
     assert_eq!(response.is_ok(), false);
     assert_eq!(response.is_err(), true);
 
-    assert!(matches!( response.err().unwrap(), error::StreamCreateError::Create {
-        stream: _, // ?
-        status: ResponseCode::StreamAlreadyExists,
-    }));
-
+    assert!(matches!(
+        response.err().unwrap(),
+        error::StreamCreateError::Create {
+            stream: _, // ?
+            status: ResponseCode::StreamAlreadyExists,
+        }
+    ));
 
     let delete_response = env.delete_stream(&stream_to_test).await;
     assert_eq!(delete_response.is_ok(), true);
@@ -36,9 +37,11 @@ async fn environment_create_stream_twice() {
     assert_eq!(delete_response.is_ok(), false);
     assert_eq!(delete_response.is_err(), true);
 
-    assert!(matches!( delete_response.err().unwrap(), error::StreamDeleteError::Delete {
-        stream: _, //
-        status: ResponseCode::StreamDoesNotExist,
-    }));
-
+    assert!(matches!(
+        delete_response.err().unwrap(),
+        error::StreamDeleteError::Delete {
+            stream: _, //
+            status: ResponseCode::StreamDoesNotExist,
+        }
+    ));
 }
