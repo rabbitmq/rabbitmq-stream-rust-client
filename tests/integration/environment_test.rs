6d603b0c2f5d72ba01e96b9fc0d796821188aebc
Gabriele Santomaggio <G.santomaggio@gmail.com>
2023-07-10 19:37:27 +0200

    add tests
    Signed-off-by: Gabriele Santomaggio <G.santomaggio@gmail.com>

--- tests/integration/environment_test.rs
+++ tests/integration/environment_test.rs
@@ -1,6 +1,35 @@
+use fake::{Fake, Faker};
+
+use rabbitmq_stream_client::{Environment, error};
+use rabbitmq_stream_protocol::ResponseCode;
+
 use crate::common::TestEnvironment;
 
 #[tokio::test(flavor = "multi_thread")]
 async fn environment_create_test() {
     let _ = TestEnvironment::create().await;
+}
+
+
+#[tokio::test(flavor = "multi_thread")]
+async fn environment_create_stream_twice() {
+    let env = Environment::builder().build().await.unwrap();
+    let stream_to_test: String = Faker.fake();
+    let response = env.stream_creator().create(&stream_to_test).await;
+    assert_eq!(response.is_ok(), true);
+
+    let response = env.stream_creator().create(&stream_to_test).await;
+    assert_eq!(response.is_ok(), false);
+    assert_eq!(response.is_err(), true);
+
+    assert!(matches!( response.err().unwrap(), error::StreamCreateError::Create {
+        stream: _,
+        status: ResponseCode::StreamAlreadyExists,
+    }));
+
+    print!("{:?}", response.err().unwrap());
+
+    let delete_response = env.delete_stream(&stream_to_test).await;
+
+    assert_eq!(delete_response.is_ok(), true);
 }
